1be8180437b3146c9ca42139a671c80578b02984
Your Name <andrewgirgis099@gmail.com>
2023-11-04 23:01:48 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,95 @@
+
+
+function moveToAboutUs()
+{
+    window.location.href = "about.html";
+}
+
+function moveToNewsDetails()
+{
+    window.location.href = "news-details.html#newsDetails";
+}
+
+function moveToNewsDetailsPage()
+{
+    window.location.href = "news-details.html";
+}
+
+function moveToNewsDetailsPage()
+{
+    window.location.href = "news-details.html";
+}
+
+function moveToHomePage()
+{
+    window.location.href = "index.html";
+}
+
+function moveToCaseDetails()
+{
+    window.location.href = "Case-Detail.html#attorney-case";
+}
+
+function moveToCaseDetailsFromCase()
+{
+    window.location.href = "../Case-Detail.html#attorney-case";
+}
+function moveToServices()
+{
+    window.location.href = "Services/services.html";
+}
+
+function moveToServiceDetails()
+{
+    window.location.href = "NorthmanBoshPart/service-details.html#professional-lawyer";
+}
+function moveToServiceDetailsEdu()
+{
+    window.location.href = "../NorthmanBoshPart/service-details.html#educationLowyer";
+}
+
+function moveToAppointment()
+{
+    window.location.href = "../appointment.html";
+}
+
+function moveToAppointmentpage()
+{
+    window.location.href = "appointment.html";
+}
+
+function moveToLowyerSeeMore()
+{
+    window.location.href = "../NorthmanBoshPart/lawyer.html";
+}
+
+function moveToLowyer()
+{
+    window.location.href = "NorthmanBoshPart/lawyer.html";
+}
+
+function moveToNewsDetails()
+{
+    window.location.href = "../news-details.html#cases-successfully";
+}
+
+function moveToNewsCases()
+{
+    window.location.href = "cases/cases.html";
+}
+
+function moveToCases()
+{
+    window.location.href = "../cases/cases.html";
+}
+
+function moveToNews()
+{
+    window.location.href = "NorthmanBoshPart/news.html";
+}
+
+function moveToNewspage()
+{
+    window.location.href = "../NorthmanBoshPart/news.html";
+}
+
